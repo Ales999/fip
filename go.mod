f0914fc222d2642c4a349b248c401fb8c4fed177
Alexey Mekhanoshin <367257+Ales999@users.noreply.github.com>
2023-12-02 00:39:18 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/ales999/fip
+
+go 1.21.3
+
+replace github.com/ales999/cisaccs => ../cisaccs
+
+require (
+	github.com/alecthomas/kong v0.8.1
+	github.com/ales999/cisaccs v0.0.12
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/knadh/koanf/parsers/yaml v0.1.0 // indirect
+	github.com/knadh/koanf/providers/file v0.1.0 // indirect
+	github.com/knadh/koanf/v2 v2.0.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/alecthomas/kong"
+
+	"github.com/ales999/cisaccs"
+)
+
+var cli struct {
+
+	// Команда для поиска ARP данных
+	Arp struct {
+		CheckHosts  []string `arg:"" name:"hosts" help:"Name of cisco hosts for finded ARP"`
+		FindIpOrMac string   `name:"find" help:"Поиск по MAC или IP"`
+	} `cmd:"" help:"Get or find ARP"`
+
+	// Команда для поиска MAC данных
+	Mac struct {
+		CheckHosts []string `arg:"" name:"hosts" help:"Name of cisco hosts for finded MAC"`
+		FindedMac  string   `name:"find" help:"Поиск по MAC"`
+	} `cmd:"" help:"Get or find MAC"`
+
+	// Больше информации при подключении
+	DebugInfo bool `name:"debug" help:"More connect debug info" short:"d"`
+	// Номер порта для ssh
+	PortSsh int `help:"SSH порт для доступа к cisco" short:"p" default:"22"`
+	// Путь к файлу конфигурации имя_cisco/группа/ip - env: CISFILE
+	CisFileName string `help:"Путь к файлу конфигурации имя_cisco/группа/ip" default:"/etc/cisco/cis.yaml" env:"CISFILE"`
+	// Путь к файлу конфигурации имя_группы/имя/пароль - env: CISPWDS
+	PwdFileName string `help:"Путь к файлу конфигурации имя_группы/имя/пароль" default:"/etc/cisco/passw.json" env:"CISPWDS"`
+}
+
+func main() {
+
+	ctx := kong.Parse(&cli,
+		kong.Name("fip"),
+		kong.Description("Find IP with ARP and MAC table"),
+		kong.UsageOnError(),
+	)
+
+	cisaccs.CisDebug = cli.DebugInfo
+
+	switch ctx.Command() {
+	case "arp <hosts>":
+		err := FipFindArpCommand()
+		ctx.FatalIfErrorf(err)
+	case "mac <hosts>":
+		err := FipFindMacCommand()
+		ctx.FatalIfErrorf(err)
+	default:
+		panic(ctx.Command())
+
+	}
+	os.Exit(0)
+}
+
+// Поиск ARP-данных
+func FipFindArpCommand() error {
+
+	// Что будем выполнять на cisco
+	cmds := []string{"sh arp"}
+
+	// Подготовка к подключению.
+	acc := cisaccs.NewCisAccount(cli.CisFileName, cli.PwdFileName)
+
+	// Если мы хотим найти конкретный IP или MAC в ARP то нужно парсить вывод
+	if len(cli.Arp.FindIpOrMac) > 0 {
+
+		for _, hst := range cli.Arp.CheckHosts {
+
+			// Получаем данные с каждого хоста
+			out, err := acc.OneCisExecuteSsh(hst, cli.PortSsh, cmds)
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+
+			found, arpsm := cisaccs.CisFindArp(out, cli.Arp.FindIpOrMac)
+			if found {
+				// Печать результата поиска
+				fmt.Printf("ARP %s found, Host: %s, Port %s, IP: %s\n", arpsm.GetMac(), hst, arpsm.GetIface(), arpsm.GetIp())
+				// ARP ищем до первого совпадения, на оствшиеся хосты можно не ходить.
+				break
+			}
+
+		}
+
+	} else {
+		out, err := acc.MultiCisExecuteSsh(cli.Arp.CheckHosts, cli.PortSsh, cmds)
+		if err != nil {
+			return err
+		}
+
+		for _, line := range out {
+			if !strings.Contains(line, "Incomplete") {
+				// Выводим всю arp-таблицу на экран
+				fmt.Println(line)
+			}
+		}
+	}
+	return nil
+
+}
+
+// Поиск MAC-данных
+func FipFindMacCommand() error {
+
+	cmds := []string{"sh mac address-table | e CPU"}
+	// Prepare cisco account
+	acc := cisaccs.NewCisAccount(cli.CisFileName, cli.PwdFileName)
+
+	if len(cli.Mac.FindedMac) > 0 {
+		// Бежим по указанным хостам
+		for _, hst := range cli.Mac.CheckHosts {
+
+			cisout, err := acc.OneCisExecuteSsh(hst, cli.PortSsh, cmds)
+			if err != nil {
+				fmt.Println("Host", hst, ":", err.Error())
+				continue
+			}
+			macfound, macstrs := cisaccs.CisFindMac(cisout, cli.Mac.FindedMac)
+			// Если что-то нашли то перебираем
+			if macfound {
+				for _, macstr := range macstrs {
+					// Печать результата поиска
+					fmt.Printf("Mac %s found, Host: %s, Port: %s, Vlan: %s\n", macstr.GetMac(), hst, macstr.GetIface(), macstr.GetVlan())
+				}
+			}
+		}
+	} else {
+		out, err := acc.MultiCisExecuteSsh(cli.Mac.CheckHosts, cli.PortSsh, cmds)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		for _, line := range out {
+			// Выводим всю mac-таблицу на экран
+			fmt.Println(line)
+		}
+
+	}
+
+	return nil
+}
